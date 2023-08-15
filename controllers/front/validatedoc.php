7fbb3387549df4efc3066cf31f190c46681f8912
Ederson Ferreira <ederson.dev@gmail.com>
2023-08-15 14:44:51 -0300

    feat: usar json encode nativo do php

--- controllers/front/validatedoc.php
+++ controllers/front/validatedoc.php
@@ -2,23 +2,23 @@
 
 class PsmodcpfValidatedocModuleFrontController extends ModuleFrontController
 {
-  public function postProcess()
-  {
-    header('Content-Type: application/json');
-    $documento = filter_input(INPUT_POST, 'documento');
-    $arrRetorno = ['status' => false];
-    if(is_null($documento)){
-      echo Tools::jsonEncode($arrRetorno);
-      exit;
+    public function postProcess()
+    {
+        header('Content-Type: application/json');
+        $documento = filter_input(INPUT_POST, 'documento');
+        $arrRetorno = ['status' => false];
+        if (is_null($documento)) {
+            echo json_encode($arrRetorno);
+            exit;
+        }
+        try {
+            $this->module->validarDocumento($documento);
+            $arrRetorno['status'] = true;
+        } catch (Exception $e) {
+            http_response_code(422);
+            $arrRetorno['error'] = $e->getMessage();
+        }
+        echo json_encode($arrRetorno);
+        exit;
     }
-    try {
-      $this->module->validarDocumento($documento);
-      $arrRetorno['status'] = true;
-    }catch(Exception $e){
-      http_response_code(422);
-      $arrRetorno['error'] = $e->getMessage();
-    }
-    echo Tools::jsonEncode($arrRetorno);
-    exit;
-  }
 }
